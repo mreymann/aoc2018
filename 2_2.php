6df459c13e78aecfddd1c565e9c3b01eb110b385
Marc Reymann <mreymann@web.de>
2018-12-20 15:53:59 +0100

    deobfuscate ;-)

--- 2_2.php
+++ 2_2.php
@@ -3,8 +3,8 @@
 $lines  = file( '2_1.in', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES );
 while ( $word = array_shift( $lines ) ) {
 	foreach ( $lines as $line ) {
-		if ( levenshtein($word, $line) == 1 ) {
-			echo join("", (array_intersect_assoc(str_split($word),str_split($line)))) . "\n";
+		if ( levenshtein( $word, $line ) == 1 ) {
+			echo join( "", ( array_intersect_assoc( str_split( $word ), str_split( $line ) ) ) ) . "\n";
 			exit;
 		}
 	}
