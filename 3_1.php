7b6a90a72ad4a22609d8e2fbb6ae976a162c9868
Marc Reymann <mreymann@web.de>
2018-12-21 09:21:48 +0100

    second loop is not needed!

--- 3_1.php
+++ 3_1.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php
 <?php
 $map = [];
+$multi = 0;
 $lines  = file( '3_1.in', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES );
 foreach ( $lines as $line_num => $line ) {
 	preg_match_all( '|^#\d+ @ (\d+),(\d+): (\d+)x(\d+)$|', $line, $matches, PREG_SET_ORDER );
@@ -9,13 +10,8 @@ foreach ( $lines as $line_num => $line ) {
 		for ( $j = $y; $j < ( $y + $h ); $j++ ) {
 			if ( !isset( $map[$i][$j] ) ) $map[$i][$j] = 0;
 			$map[$i][$j] += 1;
+			if ( $map[$i][$j] == 2 ) $multi++;
 		}
-	}
-}
-$multi = 0;
-for ( $i = min( array_keys( $map ) ); $i <= max( array_keys( $map ) ); $i++ ) {
-	for ( $j = min( array_keys( $map[$i] ) ); $j <= max( array_keys( $map[$i] ) ); $j++ ) {
-		if ( isset( $map[$i][$j] ) && $map[$i][$j] > 1 ) $multi++;
 	}
 }
 echo "$multi\n";
